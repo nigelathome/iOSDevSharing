f32f4c662a9d6d0f321fd3e96da9ca33b053f06a
nigelli <nigelli@tencent.com>
2021-08-08 17:56:52 +0800

    hello world

--- iOSAppDev/iOSAppDev/ViewController.m
+++ iOSAppDev/iOSAppDev/ViewController.m
@@ -15,8 +15,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self.view addSubview:({
+        UILabel *label = [[UILabel alloc] init];
+        label.text = @"Hello, World";
+        [label sizeToFit];
+        label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2);
+        label;
+    })];
 }
-
 
 @end
