f215496929f3a3fd46bec324783fceac31aea944
nigelli <nigelli@tencent.com>
2021-08-15 17:43:21 +0800

    usage of flow layout

--- iOSAppDev/iOSAppDev/GTVideoViewController.m
+++ iOSAppDev/iOSAppDev/GTVideoViewController.m
@@ -7,7 +7,7 @@
 
 #import "GTVideoViewController.h"
 
-@interface GTVideoViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+@interface GTVideoViewController () <UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout>
 
 @end
 
@@ -28,13 +28,18 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     
-    UICollectionViewLayout *collectionViewLayout = [[UICollectionViewFlowLayout alloc] init];
-    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:collectionViewLayout];
+    UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
+    flowLayout.minimumLineSpacing = 10;
+    flowLayout.minimumInteritemSpacing = 10;
+    flowLayout.itemSize = CGSizeMake((self.view.frame.size.width - 10)/2, 150);
+    
+    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:flowLayout];
     [collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cell_1"];
     [collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cell_2"];
     [collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cell_3"];
     collectionView.delegate = self;
     collectionView.dataSource = self;
+    
     [self.view addSubview:collectionView];
 }
 
@@ -58,6 +63,20 @@
     }
     
     return cell;
+}
+
+
+#pragma mark - UICollectionViewDelegateFlowLayout
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.item % 3 == 0) {
+        return CGSizeMake((self.view.frame.size.width - 10)/2, 100);
+    } else if (indexPath.item % 3 == 1) {
+        return CGSizeMake((self.view.frame.size.width - 10)/2, 150);
+    } else {
+        return CGSizeMake((self.view.frame.size.width - 10)/2, 200);
+    }
+    return CGSizeZero;
 }
 
 @end
