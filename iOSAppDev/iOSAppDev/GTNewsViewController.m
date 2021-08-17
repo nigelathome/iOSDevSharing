dddf5065553a0d303ebfec3521267320ca1849a8
nigelli <nigelli@tencent.com>
2021-08-17 19:32:51 +0800

    usage of UIScrollView

--- iOSAppDev/iOSAppDev/GTNewsViewController.m
+++ iOSAppDev/iOSAppDev/GTNewsViewController.m
@@ -7,7 +7,7 @@
 
 #import "GTNewsViewController.h"
 
-@interface GTNewsViewController ()
+@interface GTNewsViewController () <UIScrollViewDelegate>
 
 @end
 
@@ -28,16 +28,51 @@
     
     self.view.backgroundColor = [UIColor whiteColor];
     
+    NSArray *colorArray = @[
+        [UIColor redColor],
+        [UIColor orangeColor],
+        [UIColor yellowColor],
+        [UIColor greenColor],
+        [UIColor cyanColor],
+        [UIColor blueColor],
+        [UIColor purpleColor],
+    ];
+    
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    scrollView.contentSize = CGSizeMake(self.view.bounds.size.width * colorArray.count, self.view.bounds.size.height);
+    scrollView.scrollEnabled = YES;
+    scrollView.pagingEnabled = YES;
+    scrollView.showsVerticalScrollIndicator = NO;
+    for (NSUInteger i = 0; i < colorArray.count; i++) {
+        [scrollView addSubview:({
+            UIView *v = [[UIView alloc] initWithFrame:CGRectMake(scrollView.frame.size.width * i, 0, scrollView.frame.size.width, scrollView.frame.size.height)];
+            v.backgroundColor = [colorArray objectAtIndex:i];
+            v;
+        })];
+    }
+    scrollView.delegate = self;
+    [self.view addSubview:scrollView];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - UIScrollViewDelegate
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    printf("scrollViewDidScroll \n");
 }
-*/
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
+    printf("scrollViewWillBeginDragging \n");
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate {
+    printf("scrollViewDidEndDragging: \n");
+}
+
+- (void)scrollViewWillBeginDecelerating:(UIScrollView *)scrollView {
+    printf("scrollViewWillBeginDecelerating: \n");
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+    printf("scrollViewDidEndDecelerating: \n");
+}
 
 @end
